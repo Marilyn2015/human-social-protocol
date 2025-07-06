aa4ca13b019dfee976a7612edaedb6fbcd975f9c
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-07-06 18:58:06 -0400

    Create HumanProof.sol

---
+++ contracts/HumanProof.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HumanProof {
+    mapping(address => bool) public verifiedHumans;
+
+    function verifyHuman(address user) public {
+        verifiedHumans[user] = true;
+    }
+
+    function isHuman(address user) public view returns (bool) {
+        return verifiedHumans[user];
+    }
+}

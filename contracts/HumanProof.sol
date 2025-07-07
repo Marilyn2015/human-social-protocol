a527cff173612f48acd3231ca98cbc93108e0fb8
Marilyn Stephens <phoenixbird83@hotmail.com>
2025-07-06 20:03:14 -0400

    Update HumanProof.sol

--- contracts/HumanProof.sol
+++ contracts/HumanProof.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.0;
 
 contract HumanProof {
-    mapping(address => bool) public verified;
+    // contract logic
+}
 
     function verifyHuman(address user) public {
         verified[user] = true;
